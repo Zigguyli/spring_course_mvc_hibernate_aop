85de11144d5478d35d0972c13f6536ab95eaad95
Ilgiz Garafiev <ilgizgarafiev4@gmail.com>
2024-08-05 10:01:30 +0300

    Lesson 71 with modern pom.xml

---
+++ src/main/java/com/ilgizgarafiev/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.ilgizgarafiev.spring.mvc_hibernate_aop.service;
+
+import com.ilgizgarafiev.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+}
